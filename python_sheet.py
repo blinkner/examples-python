75781ee8997a7252fcd45f02c681ab7d88c50b87
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-01 08:19:16 -0300

    Update

---
+++ python_sheet.py
@@ -0,0 +1,9 @@
+import numpy
+import math
+import random
+
+random.random() # Inteiro aleatório entre 0 e 1.
+random.uniform(inicio, fim) # Inteiro aleatório entre início e fim.
+random.randint(inicio, fim)
+random.randrange(inicio, fim, passo)
+
