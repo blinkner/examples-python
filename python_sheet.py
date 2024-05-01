b649a25ffc9a8e33c10e4411a38adb9322272b42
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-01 09:22:23 -0300

    Update python_sheet.py

--- python_sheet.py
+++ python_sheet.py
@@ -2,8 +2,16 @@ import numpy
 import math
 import random
 
-random.random() # Inteiro aleatório entre 0 e 1.
-random.uniform(inicio, fim) # Inteiro aleatório entre início e fim.
-random.randint(inicio, fim)
-random.randrange(inicio, fim, passo)
+random.random() # Float aleatório entre 0 e 1.
+random.uniform(inicio, fim) # Float aleatório entre início e fim.
+random.randint(inicio, fim) # Inteiro aleatório entre início e fim.
+random.randrange(inicio, fim, passo) # Inteiro aleatório entre início e fim com passo.
+range(inicio, fim, passo)
+list()
+input("Entre com um valor: ") # Entrada de dados
+
+
+
+def funcao(): # Função
+    return 0
 
