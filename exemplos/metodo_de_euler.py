4c2a26b0318986fb34d9afbd5d5fdb4b97dd8b72
blinkner <gabroc9@gmail.com>
2026-06-16 16:38:44 -0300

    update functions

--- exemplos/metodo_de_euler.py
+++ exemplos/metodo_de_euler.py
@@ -6,32 +6,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.close('all')
 
-f = lambda x, y: x - y + 2 # EDO
-h = 0.1 # Passo
-x_i = 0 # Intervalo inicial
-x_f = 1 # Invervalo final
-n = (int) ((x_f - x_i) / h) # Quantidade de subintervalos
-
-x = np.empty(n + 1)
-x.fill(np.nan)
-y = np.empty(n + 1)
-y.fill(np.nan)
-
-x[0] = 0.0
-y[0] = 2.0
-
-print(x[0], ',', y[0])
-
-for j in range(n):
-    y[j+1] = y[j] + h * f(x[j], y[j])
+def metodo_de_euler(f, h, x_i, x_f, x0, y0):
+    # f: EDO
+    # h: passo
+    # x_i: intervalo inicial
+    # x_f: intervalo final
+    # x_0: Valor inicial de x
+    # y0: Valor inicial de y
     
-    x[j+1] = x[j] + h
+    n = (int) ((x_f - x_i) / h) # Quantidade de subintervalos
 
-    print(x[j+1], ',', y[j+1])
+    x = np.empty(n + 1)
+    x.fill(np.nan)
+    y = np.empty(n + 1)
+    y.fill(np.nan)
+
+    x[0] = x0
+    y[0] = y0
+
+    print(x[0], ',', y[0])
+
+    for j in range(n):
+        y[j+1] = y[j] + h * f(x[j], y[j])
+        
+        x[j+1] = x[j] + h
+
+        print(x[j+1], ',', y[j+1])
 
 
-n = np.linspace(0, 1, n + 1, endpoint=True)
-plt.figure()
-plt.plot(n, y, 'r')
-plt.grid()
-plt.show()
+    n = np.linspace(0, 1, n + 1, endpoint=True)
+    plt.figure()
+    plt.plot(n, y, 'r')
+    plt.grid()
+    plt.show()
+
+metodo_de_euler(lambda x, y: x - y + 2, 0.1, 0, 1, 0.0, 2.0)
--- python_sheet.py
+++ python_sheet.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 import random
 from functools import reduce
 
